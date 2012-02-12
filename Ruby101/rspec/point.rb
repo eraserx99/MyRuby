e2afc8f9a40fb5b887c2c48abda3d8a49e9be489
vagrant <vagrant@vagrant.(none)>
2012-02-12 13:52:08 -0500

    start to try rsepc

---
+++ Ruby101/rspec/point.rb
@@ -0,0 +1,40 @@
+
+class Point
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def +(other) # Define + to do vector addition
+    Point.new(@x + other.x, @y + other.y)
+  end
+
+  def -@ # Define unary minus to negate both coordinates
+    Point.new(-@x, -@y)
+  end
+
+  def *(scalar) # Define * to perform scalar multiplication
+    Point.new(@x*scalar, @y*scalar)
+  end
+
+  def coerce(other)
+    p "coerce function of Point"
+    [self, other]
+  end
+
+  def Point.here()
+  end
+
+  def Point.sum(*points)
+  end
+
+  def hi()
+  end
+  private :hi
+
+  class << Point
+  end
+
+end
---
+++ Ruby101/rspec/point_spec.rb
@@ -0,0 +1,8 @@
+require "./point"
+
+describe Point do
+  it "hi should be a private method" do
+    
+  end
+  
+end
