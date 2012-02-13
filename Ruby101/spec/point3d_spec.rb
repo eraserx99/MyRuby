287f1e93fd7977174213a18320a79b468d1c7617
eraserx99 <eraserx99@gmail.com>
2012-02-12 22:05:44 -0500

    adjust the structure

--- Ruby101/spec/point3d_spec.rb
+++ Ruby101/spec/point3d_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe Point3D do
   it "Point3D can be initilized with x, y, and z coordinates" do
     point = Point3D.new(100, 200, 300)
-
+    point.x.should eq(100)
+    point.y.should eq(200)
+    point.z.should eq(300)
   end
 end
